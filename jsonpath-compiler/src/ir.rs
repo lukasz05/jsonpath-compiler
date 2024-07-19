a990f20c2e31b8a71c4606396b41d80e8c74ac07
Łukasz Orawiec <orawiec@mailbox.org>
2024-07-19 16:55:41 +0200

    Add comments describing the IR

--- jsonpath-compiler/src/ir.rs
+++ jsonpath-compiler/src/ir.rs
@@ -3,6 +3,8 @@ use std::fmt::{Debug, Display, Formatter};
 
 pub mod generator;
 
+/// An intermediate representation of a JSONPath query.
+/// It consists of segments, which correspond to the segments of the original query.
 #[derive(Debug)]
 pub struct Query {
     segments: Vec<Segment>,
@@ -24,6 +26,13 @@ impl Display for Query {
     }
 }
 
+
+/// Each segment is a sequence of instructions which is executed for each node of its input nodelist.
+/// Instructions operate on a stack which is initialized with the current input node before each
+/// execution. Instructions can add nodes to the segment's output nodelist.
+/// After the segment has been executed for each input node, its output nodelist becomes an input
+/// nodelist for the next segment. The input nodelist of the first segment consists of the root node
+/// of the query argument. The output nodelist of the last segment is the result of the query.
 #[derive(Debug)]
 pub struct Segment {
     instructions: Vec<Instruction>,
@@ -51,29 +60,58 @@ impl Display for Segment {
 
 #[derive(Debug)]
 pub enum Instruction {
+    /// Pushes the given literal value onto the stack.
     PushLiteral { literal: Literal },
+    /// Pushes the root node onto the stack.
     PushRootNode,
-
+    /// Pops a node from the stack and pushes all its children (elements/members) onto the stack.
     PopAndPushAllChildren,
+    /// Pops a node from the stack and pushes its member with the given name onto the stack.
+    /// If the popped node is not an object, or it does not have a member with the given name,
+    /// a special null node is pushed onto the stack.
     PopAndPushChildByName { name: Name },
+    /// Pops a node from the stack and pushes its element at the given index onto the stack.
+    /// If the popped node is not an array, or it does not have the given index, a special null node
+    /// is pushed onto the stack.
     PopAndPushElementAtIndex { index: Index },
-
+    /// Adds all children (elements/members) of the node at the top of the stack to the current
+    /// segment's output nodelist.
     SelectAllChildren,
+    /// Reads the node at the top of the stack, and if it is an object that has a member with
+    /// the given name, adds the member to the current segment's output nodelist.
     SelectChildByName { name: Name },
+    /// Reads the node at the top of the stack, and if it is an array that has an element at the
+    /// given index, adds the element to the current segment's output nodelist.
     SelectElementAtIndex { index: Index },
+    /// Reads the node at the top of the stack, and if it is an array, adds its elements specified
+    /// by the given slice expression to the current segment's output nodelist.
     SelectSlice { slice: Slice },
+    /// Pops a logical value from the stack, reads the node at the top of the stack,
+    /// and if the value is true and the node is not the null node, adds it to the current segment's
+    /// output nodelist.
     SelectNodeConditionally,
-
+    /// Executes the given instructions in a loop until the stack is empty.
     WhileStackNotEmpty { instructions: Vec<Instruction> },
-
-    ExistenceTest { absolute: bool, subquery: Query },
-    Compare { op: ComparisonOp },
-    And,
-    Or,
-    Not,
-
+    /// Executes the given instructions in a loop for each child (element/member) of the node at
+    /// the top of the stack.
+    /// During the iteration, the current node is available via `PushCurrentFilterNode` instruction.
     FilterIteration { instructions: Vec<Instruction> },
+    /// Pushes the current node of the filter iteration onto the stack.
     PushCurrentFilterNode,
+    /// Executes the given subquery in a separate execution context and pushes the `true` value if
+    /// the resultant nodelist is non-empty, otherwise pushes the `false` value.
+    /// The input nodelist of the first segment of the subquery consists of the root node or the
+    /// current node of the filter iteration, depending on the value of `absolute`.
+    ExistenceTest { absolute: bool, subquery: Query },
+    /// Pops two values from the stack, applies the given comparison operator to them and pushes
+    /// the resultant logical value onto the stack.
+    Compare { op: ComparisonOp },
+    /// Pops two logical values from the stack and pushes their conjunction onto the stack.
+    And,
+    /// Pops two logical values from the stack and pushes their disjunction onto the stack.
+    Or,
+    /// Pops a logical value from the stack and pushes its negation onto the stack.
+    Not
 }
 
 impl Instruction {
