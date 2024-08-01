314caac9d886a12a9f4216a071c6030547423cd1
Łukasz Orawiec <orawiec@mailbox.org>
2024-08-01 20:23:50 +0200

    Rework the IR

--- jsonpath-compiler/src/ir.rs
+++ jsonpath-compiler/src/ir.rs
@@ -1,10 +1,9 @@
 use std::fmt;
 use std::fmt::{Debug, Display, Formatter};
+use std::ops::Deref;
 
 pub mod generator;
 
-/// An intermediate representation of a JSONPath query.
-/// It consists of segments, which correspond to the segments of the original query.
 #[derive(Debug)]
 pub struct Query {
     segments: Vec<Segment>,
@@ -26,23 +25,25 @@ impl Display for Query {
     }
 }
 
-
-/// Each segment is a sequence of instructions which is executed for each node of its input nodelist.
-/// Instructions operate on a stack which is initialized with the current input node before each
-/// execution. Instructions can add nodes to the segment's output nodelist.
-/// After the segment has been executed for each input node, its output nodelist becomes an input
-/// nodelist for the next segment. The input nodelist of the first segment consists of the root node
-/// of the query argument. The output nodelist of the last segment is the result of the query.
 #[derive(Debug)]
-pub struct Segment {
-    instructions: Vec<Instruction>,
+pub enum Segment {
+    ChildSegment { selectors: Vec<Selector> },
+    DescendantSegment { selectors: Vec<Selector> },
 }
 
 impl Segment {
     fn fmt(&self, f: &mut Formatter, indent: u16) -> fmt::Result {
         write_indent(f, indent)?;
-        write!(f, "Segment {{\n")?;
-        for instruction in &self.instructions {
+        let mut segment_selectors: &Vec<Selector> = &vec![];
+        if let Segment::ChildSegment { selectors } = self {
+            write!(f, "ChildSegment {{\n")?;
+            segment_selectors = selectors;
+        }
+        if let Segment::DescendantSegment { selectors } = self {
+            write!(f, "DescendantSegment {{\n")?;
+            segment_selectors = selectors;
+        }
+        for instruction in segment_selectors {
             instruction.fmt(f, indent + 1)?;
             write!(f, "\n")?;
         }
@@ -57,111 +58,159 @@ impl Display for Segment {
     }
 }
 
-
 #[derive(Debug)]
-pub enum Instruction {
-    /// Pushes the given literal value onto the stack.
-    PushLiteral { literal: Literal },
-    /// Pushes the root node onto the stack.
-    PushRootNode,
-    /// Pops a node from the stack and pushes all its children (elements/members) onto the stack.
-    PopAndPushAllChildren,
-    /// Pops a node from the stack and pushes its member with the given name onto the stack.
-    /// If the popped node is not an object, or it does not have a member with the given name,
-    /// a special null node is pushed onto the stack.
-    PopAndPushChildByName { name: Name },
-    /// Pops a node from the stack and pushes its element at the given index onto the stack.
-    /// If the popped node is not an array, or it does not have the given index, a special null node
-    /// is pushed onto the stack.
-    PopAndPushElementAtIndex { index: Index },
-    /// Adds all children (elements/members) of the node at the top of the stack to the current
-    /// segment's output nodelist.
-    SelectAllChildren,
-    /// Reads the node at the top of the stack, and if it is an object that has a member with
-    /// the given name, adds the member to the current segment's output nodelist.
-    SelectChildByName { name: Name },
-    /// Reads the node at the top of the stack, and if it is an array that has an element at the
-    /// given index, adds the element to the current segment's output nodelist.
-    SelectElementAtIndex { index: Index },
-    /// Reads the node at the top of the stack, and if it is an array, adds its elements specified
-    /// by the given slice expression to the current segment's output nodelist.
-    SelectSlice { slice: Slice },
-    /// Pops a logical value from the stack, reads the node at the top of the stack,
-    /// and if the value is true and the node is not the null node, adds it to the current segment's
-    /// output nodelist.
-    SelectNodeConditionally,
-    /// Executes the given instructions in a loop until the stack is empty.
-    WhileStackNotEmpty { instructions: Vec<Instruction> },
-    /// Executes the given instructions in a loop for each child (element/member) of the node at
-    /// the top of the stack.
-    /// During the iteration, the current node is available via `PushCurrentFilterNode` instruction.
-    FilterIteration { instructions: Vec<Instruction> },
-    /// Pushes the current node of the filter iteration onto the stack.
-    PushCurrentFilterNode,
-    /// Executes the given subquery in a separate execution context and pushes the `true` value if
-    /// the resultant nodelist is non-empty, otherwise pushes the `false` value.
-    /// The input nodelist of the first segment of the subquery consists of the root node or the
-    /// current node of the filter iteration, depending on the value of `absolute`.
-    ExistenceTest { absolute: bool, subquery: Query },
-    /// Pops two values from the stack, applies the given comparison operator to them and pushes
-    /// the resultant logical value onto the stack.
-    Compare { op: ComparisonOp },
-    /// Pops two logical values from the stack and pushes their conjunction onto the stack.
-    And,
-    /// Pops two logical values from the stack and pushes their disjunction onto the stack.
-    Or,
-    /// Pops a logical value from the stack and pushes its negation onto the stack.
-    Not
+pub enum Selector {
+    /// Selects all children (elements/members) of the node.
+    AllChildren,
+    /// If the node is an object that has a member with the given name, selects the member.
+    ChildByName { name: Name },
+    /// If the node is an array that has an element at the given index, selects the element.
+    ElementAtIndex { index: Index },
+    /// If the node is an array, selects its elements specified by the given slice expression.
+    Slice { slice: Slice },
+    // Selects all children satisfying the given filter expressoin.
+    Filter { logical_expression: LogicalExpression },
 }
 
-impl Instruction {
+impl Selector {
     fn fmt(&self, f: &mut Formatter, indent: u16) -> fmt::Result {
         write_indent(f, indent)?;
         match self {
-            Instruction::PushLiteral { literal } => write!(f, "PushLiteral({literal})"),
-            Instruction::PushRootNode => write!(f, "PushRootNode"),
-            Instruction::PopAndPushAllChildren => write!(f, "PopAndPushAllChildren"),
-            Instruction::PopAndPushChildByName { name } => write!(f, "PopAndPushChildByName({name})"),
-            Instruction::PopAndPushElementAtIndex { index } => write!(f, "PopAndPushElementAtIndex({index})"),
-            Instruction::SelectAllChildren => write!(f, "SelectAllChildren"),
-            Instruction::SelectChildByName { name } => write!(f, "SelectChildByName({name})"),
-            Instruction::SelectElementAtIndex { index } => write!(f, "SelectElementAtIndex({index})"),
-            Instruction::SelectSlice { slice } => write!(f, "SelectSlice({slice})"),
-            Instruction::SelectNodeConditionally => write!(f, "SelectNodeConditionally"),
-            Instruction::WhileStackNotEmpty { instructions } => {
-                write!(f, "WhileStackNotEmpty {{\n")?;
-                for instruction in instructions {
-                    instruction.fmt(f, indent + 1)?;
-                    write!(f, "\n")?;
-                }
+            Selector::AllChildren => write!(f, "SelectAllChildren"),
+            Selector::ChildByName { name } => write!(f, "SelectChildByName({name})"),
+            Selector::ElementAtIndex { index } => write!(f, "SelectElementAtIndex({index})"),
+            Selector::Slice { slice } => write!(f, "SelectSlice({slice})"),
+            Selector::Filter { logical_expression } => {
+                write!(f, "Filter(\n")?;
+                logical_expression.fmt(f, indent + 1)?;
+                write!(f, "\n")?;
                 write_indent(f, indent)?;
-                write!(f, "}}")
+                write!(f, ")")
             }
-            Instruction::ExistenceTest { absolute, subquery } => {
-                write!(f, "ExistenceTest({}) {{\n", if *absolute { "absolute" } else { "relative" })?;
-                subquery.fmt(f, indent + 1)?;
-                write_indent(f, indent)?;
-                write!(f, "}}")
-            }
-            Instruction::Compare { op } => write!(f, "Compare({op})"),
-            Instruction::And => write!(f, "And"),
-            Instruction::Or => write!(f, "Or"),
-            Instruction::Not => write!(f, "Not"),
-            Instruction::FilterIteration { instructions } => {
-                write!(f, "FilterIteration {{\n")?;
-                for instruction in instructions {
-                    instruction.fmt(f, indent + 1)?;
-                    write!(f, "\n")?;
-                }
-                write_indent(f, indent)?;
-                write!(f, "}}")
-            }
-            Instruction::PushCurrentFilterNode => write!(f, "PushCurrentFilterNode")
         }
     }
 }
 
-impl Display for Instruction {
+impl Display for Selector {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        self.fmt(f, 0)
+    }
+}
+
+#[derive(Debug)]
+pub enum SingularSelector {
+    /// If the node is an object that has a member with the given name, selects the member.
+    ChildByName { name: Name },
+    /// If the node is an array that has an element at the given index, selects the element.
+    ElementAtIndex { index: Index },
+}
+
+impl SingularSelector {
+    fn fmt(&self, f: &mut Formatter, indent: u16) -> fmt::Result {
+        write_indent(f, indent)?;
+        match self {
+            SingularSelector::ChildByName { name } => write!(f, "SelectChildByName({name})"),
+            SingularSelector::ElementAtIndex { index } => write!(f, "SelectElementAtIndex({index})"),
+        }
+    }
+}
+
+impl Display for SingularSelector {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        self.fmt(f, 0)
+    }
+}
+
+#[derive(Debug)]
+pub enum LogicalExpression {
+    Or { lhs: Box<LogicalExpression>, rhs: Box<LogicalExpression> },
+    And { lhs: Box<LogicalExpression>, rhs: Box<LogicalExpression> },
+    Not { expr: Box<LogicalExpression> },
+    Comparison { lhs: Comparable, rhs: Comparable, op: ComparisonOp },
+    ExistenceTest { absolute: bool, subquery: Query },
+}
+
+impl LogicalExpression {
+    fn fmt(&self, f: &mut Formatter, indent: u16) -> fmt::Result {
+        write_indent(f, indent)?;
+        match self {
+            LogicalExpression::Or { lhs, rhs } => {
+                LogicalExpression::fmt_binop(f, indent, "Or", (*lhs).deref(), (*rhs).deref())
+            }
+            LogicalExpression::And { lhs, rhs } => {
+                LogicalExpression::fmt_binop(f, indent, "And", (*lhs).deref(), (*rhs).deref())
+            }
+            LogicalExpression::Not { expr } => {
+                write!(f, "Not(\n")?;
+                (*expr).deref().fmt(f, indent + 1)?;
+                write!(f, "\n")?;
+                write_indent(f, indent)?;
+                write!(f, ")")
+            }
+            LogicalExpression::Comparison { lhs, rhs, op } => {
+                write!(f, "{op}(\n")?;
+                lhs.fmt(f, indent + 1)?;
+                write!(f, ",\n")?;
+                rhs.fmt(f, indent + 1)?;
+                write!(f, "\n")?;
+                write_indent(f, indent)?;
+                write!(f, ")")
+            }
+            LogicalExpression::ExistenceTest { absolute, subquery } => {
+                write!(f, "{}ExistenceTest(\n", if *absolute { "Absolute" } else { "Relative" })?;
+                subquery.fmt(f, indent + 1)?;
+                write_indent(f, indent)?;
+                write!(f, ")")
+            }
+        }
+    }
+
+    fn fmt_binop(f: &mut Formatter, indent: u16, op: &str, lhs: &LogicalExpression, rhs: &LogicalExpression) -> fmt::Result {
+        write!(f, "{op}(\n")?;
+        lhs.fmt(f, indent + 1)?;
+        write!(f, ",\n")?;
+        rhs.fmt(f, indent + 1)?;
+        write!(f, "\n")?;
+        write_indent(f, indent)?;
+        write!(f, ")")
+    }
+}
+
+impl Display for LogicalExpression {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        self.fmt(f, 0)
+    }
+}
+
+#[derive(Debug)]
+pub enum Comparable {
+    SingularQuery { absolute: bool, selectors: Vec<SingularSelector> },
+    Literal { literal: Literal },
+}
+
+impl Comparable {
+    fn fmt(&self, f: &mut Formatter, indent: u16) -> fmt::Result {
+        write_indent(f, indent)?;
+        match self {
+            Comparable::SingularQuery { absolute, selectors } => {
+                write!(f, "{}SingularQuery(", if *absolute { "Absolute" } else { "Relative" })?;
+                if selectors.len() > 0 {
+                    write!(f, "\n")?;
+                    for selector in selectors {
+                        selector.fmt(f, indent + 1)?;
+                        write!(f, "\n")?;
+                    }
+                    write_indent(f, indent)?;
+                }
+                write!(f, ")")
+
+            }
+            Comparable::Literal { literal } => write!(f, "{}", literal),
+        }
+    }
+}
+
+impl Display for Comparable {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         self.fmt(f, 0)
     }
--- jsonpath-compiler/src/ir/generator.rs
+++ jsonpath-compiler/src/ir/generator.rs
@@ -1,12 +1,9 @@
-use std::iter;
-
-use crate::ir::{ComparisonOp, Index, Instruction, Literal, Name, Query, Segment, Slice};
-use crate::ir::Instruction::{And, Compare, ExistenceTest, FilterIteration, Not, Or,
-                             PopAndPushAllChildren, PopAndPushChildByName, PopAndPushElementAtIndex,
-                             PushCurrentFilterNode, PushLiteral, PushRootNode, SelectAllChildren,
-                             SelectChildByName, SelectElementAtIndex, SelectNodeConditionally,
-                             SelectSlice, WhileStackNotEmpty};
+use crate::ir::{Comparable, ComparisonOp, Index, Literal, LogicalExpression, Name, Query, Segment, Selector, SingularSelector, Slice};
+use crate::ir::Comparable::SingularQuery;
 use crate::ir::Literal::{Bool, Float, Int, Null, String};
+use crate::ir::LogicalExpression::{And, Comparison, ExistenceTest, Not, Or};
+use crate::ir::Segment::{ChildSegment, DescendantSegment};
+use crate::ir::Selector::{AllChildren, ChildByName, ElementAtIndex, Filter};
 
 pub fn generate(query_syntax: &rsonpath_syntax::JsonPathQuery) -> Query {
     Query {
@@ -18,82 +15,57 @@ pub fn generate(query_syntax: &rsonpath_syntax::JsonPathQuery) -> Query {
 
 fn generate_segment(segment_syntax: &rsonpath_syntax::Segment) -> Segment {
     match segment_syntax {
-        rsonpath_syntax::Segment::Child(selectors) => generate_child_segment(selectors),
-        rsonpath_syntax::Segment::Descendant(selectors) =>
-            generate_descendant_segment(selectors),
-    }
-}
-
-fn generate_child_segment(selectors: &rsonpath_syntax::Selectors) -> Segment {
-    Segment { instructions: generate_selectors(selectors) }
-}
-
-fn generate_descendant_segment(selectors: &rsonpath_syntax::Selectors) -> Segment {
-    Segment {
-        instructions: vec![
-            WhileStackNotEmpty {
-                instructions: generate_selectors(selectors).into_iter()
-                    .chain(vec![PopAndPushAllChildren])
-                    .collect()
-            },
-        ]
-    }
-}
-
-fn generate_selectors(selectors: &rsonpath_syntax::Selectors) -> Vec<Instruction> {
-    let mut instructions: Vec<Instruction> = Vec::new();
-    for selector in selectors.iter() {
-        instructions.extend(generate_selector(selector));
-    }
-    instructions
-}
-
-fn generate_selector(selector_syntax: &rsonpath_syntax::Selector) -> Vec<Instruction> {
-    match selector_syntax {
-        rsonpath_syntax::Selector::Name(name_syntax) => {
-            let name = Name(name_syntax.unquoted().to_owned());
-            vec![SelectChildByName { name }]
+        rsonpath_syntax::Segment::Child(selectors) => {
+            ChildSegment { selectors: generate_selectors(selectors) }
         }
-        rsonpath_syntax::Selector::Wildcard => vec![SelectAllChildren],
-        rsonpath_syntax::Selector::Index(index_syntax) => {
-            let index = generate_index(index_syntax);
-            vec![SelectElementAtIndex { index }]
-        }
-        rsonpath_syntax::Selector::Slice(slice_syntax) => {
-            let slice = generate_slice(slice_syntax);
-            vec![SelectSlice { slice }]
-        }
-        rsonpath_syntax::Selector::Filter(logical_expr_syntax) => {
-            vec![
-                FilterIteration {
-                    instructions: iter::once(PushCurrentFilterNode)
-                        .chain(generate_logical_expr(logical_expr_syntax))
-                        .chain(vec![SelectNodeConditionally])
-                        .collect()
-                },
-            ]
+        rsonpath_syntax::Segment::Descendant(selectors) => {
+            DescendantSegment { selectors: generate_selectors(selectors) }
         }
     }
 }
 
-fn generate_logical_expr(logical_expr_syntax: &rsonpath_syntax::LogicalExpr) -> Vec<Instruction> {
+fn generate_selectors(selectors_syntax: &rsonpath_syntax::Selectors) -> Vec<Selector> {
+    let mut selectors: Vec<Selector> = Vec::new();
+    for selector in selectors_syntax.iter() {
+        match selector {
+            rsonpath_syntax::Selector::Name(name_syntax) => {
+                let name = Name(name_syntax.unquoted().to_owned());
+                selectors.push(ChildByName { name })
+            }
+            rsonpath_syntax::Selector::Wildcard => selectors.push(AllChildren),
+            rsonpath_syntax::Selector::Index(index_syntax) => {
+                let index = generate_index(index_syntax);
+                selectors.push(ElementAtIndex { index })
+            }
+            rsonpath_syntax::Selector::Slice(slice_syntax) => {
+                let slice = generate_slice(slice_syntax);
+                selectors.push(Selector::Slice { slice })
+            }
+            rsonpath_syntax::Selector::Filter(logical_expr_syntax) => {
+                let logical_expression = generate_logical_expr(logical_expr_syntax);
+                selectors.push(Filter { logical_expression })
+            }
+        }
+    }
+    return selectors;
+}
+
+fn generate_logical_expr(logical_expr_syntax: &rsonpath_syntax::LogicalExpr) -> LogicalExpression {
     match logical_expr_syntax {
         rsonpath_syntax::LogicalExpr::Or(lhs, rhs) => {
-            generate_logical_expr(lhs).into_iter()
-                .chain(generate_logical_expr(rhs))
-                .chain(iter::once(Or))
-                .collect()
+            Or {
+                lhs: Box::new(generate_logical_expr(lhs)),
+                rhs: Box::new(generate_logical_expr(rhs)),
+            }
         }
         rsonpath_syntax::LogicalExpr::And(lhs, rhs) => {
-            generate_logical_expr(lhs).into_iter()
-                .chain(generate_logical_expr(rhs))
-                .chain(iter::once(And))
-                .collect()
+            And {
+                lhs: Box::new(generate_logical_expr(lhs)),
+                rhs: Box::new(generate_logical_expr(rhs)),
+            }
         }
         rsonpath_syntax::LogicalExpr::Not(expr) => {
-            generate_logical_expr(expr).into_iter()
-                .chain(iter::once(Not))
-                .collect()
+            Not { expr: Box::new(generate_logical_expr(expr)) }
         }
         rsonpath_syntax::LogicalExpr::Comparison(comparison_expr) => {
             generate_comparison(comparison_expr)
@@ -104,27 +76,30 @@ fn generate_logical_expr(logical_expr_syntax: &rsonpath_syntax::LogicalExpr) -> 
     }
 }
 
-fn generate_comparison(comparison_expr_syntax: &rsonpath_syntax::ComparisonExpr) -> Vec<Instruction> {
-    generate_comparable(comparison_expr_syntax.lhs()).into_iter()
-        .chain(generate_comparable(comparison_expr_syntax.rhs()))
-        .chain(iter::once(Compare { op: generate_comparison_op(&comparison_expr_syntax.op()) }))
-        .collect()
+fn generate_comparison(comparison_expr_syntax: &rsonpath_syntax::ComparisonExpr) -> LogicalExpression {
+    Comparison {
+        lhs: generate_comparable(comparison_expr_syntax.lhs()),
+        rhs: generate_comparable(comparison_expr_syntax.rhs()),
+        op: generate_comparison_op(&comparison_expr_syntax.op()),
+    }
 }
 
-fn generate_comparable(comparable_syntax: &rsonpath_syntax::Comparable) -> Vec<Instruction> {
+fn generate_comparable(comparable_syntax: &rsonpath_syntax::Comparable) -> Comparable {
     match comparable_syntax {
         rsonpath_syntax::Comparable::Literal(literal_syntax) => {
-            vec![PushLiteral { literal: generate_literal(literal_syntax) }]
+            Comparable::Literal { literal: generate_literal(literal_syntax) }
         }
         rsonpath_syntax::Comparable::AbsoluteSingularQuery(singular_query_syntax) => {
-            iter::once(PushRootNode)
-                .chain(generate_singular_query(singular_query_syntax))
-                .collect()
+            SingularQuery {
+                absolute: true,
+                selectors: generate_singular_query(singular_query_syntax),
+            }
         }
         rsonpath_syntax::Comparable::RelativeSingularQuery(singular_query_syntax) => {
-            iter::once(PushCurrentFilterNode)
-                .chain(generate_singular_query(singular_query_syntax))
-                .collect()
+            SingularQuery {
+                absolute: false,
+                selectors: generate_singular_query(singular_query_syntax),
+            }
         }
     }
 }
@@ -140,28 +115,33 @@ fn generate_comparison_op(comparison_op_syntax: &rsonpath_syntax::ComparisonOp) 
     }
 }
 
-fn generate_existence_test(test_expr: &rsonpath_syntax::TestExpr) -> Vec<Instruction> {
+fn generate_existence_test(test_expr: &rsonpath_syntax::TestExpr) -> LogicalExpression {
     match test_expr {
         rsonpath_syntax::TestExpr::Absolute(query_syntax) => {
-            vec![ExistenceTest { absolute: true, subquery: generate(query_syntax) }]
+            ExistenceTest {
+                absolute: true,
+                subquery: generate(query_syntax),
+            }
         }
         rsonpath_syntax::TestExpr::Relative(query_syntax) => {
-            vec![ExistenceTest { absolute: false, subquery: generate(query_syntax) }]
+            ExistenceTest {
+                absolute: false,
+                subquery: generate(query_syntax),
+            }
         }
     }
 }
 
-
-fn generate_singular_query(singular_query_syntax: &rsonpath_syntax::SingularJsonPathQuery) -> Vec<Instruction> {
+fn generate_singular_query(singular_query_syntax: &rsonpath_syntax::SingularJsonPathQuery) -> Vec<SingularSelector> {
     singular_query_syntax.segments().map(|singular_segment_syntax| {
         match singular_segment_syntax {
             rsonpath_syntax::SingularSegment::Name(name_syntax) => {
                 let name = Name(name_syntax.unquoted().to_owned());
-                PopAndPushChildByName { name }
+                SingularSelector::ChildByName { name }
             }
             rsonpath_syntax::SingularSegment::Index(index_syntax) => {
                 let index = generate_index(index_syntax);
-                PopAndPushElementAtIndex { index }
+                SingularSelector::ElementAtIndex { index }
             }
         }
     }).collect()
@@ -175,7 +155,7 @@ fn generate_slice(slice_syntax: &rsonpath_syntax::Slice) -> Slice {
     Slice {
         start: index_syntax_as_i64(&slice_syntax.start()),
         end: slice_syntax.end().map(|index_syntax| index_syntax_as_i64(&index_syntax)),
-        step: step_syntax_as_i64(&slice_syntax.step()),
+        step: step_syntax_as_i64(&slice_syntax.step())
     }
 }
 
@@ -187,7 +167,7 @@ fn generate_literal(literal_syntax: &rsonpath_syntax::Literal) -> Literal {
             rsonpath_syntax::num::JsonNumber::Float(x) => Float(x.as_f64()),
         },
         rsonpath_syntax::Literal::Bool(bool) => Bool(bool.to_owned()),
-        rsonpath_syntax::Literal::Null => Null,
+        rsonpath_syntax::Literal::Null => Null
     }
 }
 
